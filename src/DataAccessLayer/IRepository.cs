fdd97a30be7cc9c1de3f8900e3ff78c767600719
Shelyahin_Mihail <55269575+GervantFromRibli@users.noreply.github.com>
2020-09-04 23:31:37 +0300

    Create DAL
    Create data access layer, add repository interface with basic methods and make some replacements with previous classes

---
+++ src/DataAccessLayer/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public interface IRepository<T> where T : class
+    {
+        void Create(T item);
+        T FindById(int id);
+        IEnumerable<T> Get();
+        IEnumerable<T> Get(Func<T, bool> predicate);
+        void Remove(T item);
+        void Update(T item);
+    }
+}
