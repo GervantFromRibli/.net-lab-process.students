f4a941b0c12814ef7925f68390bd5565c58123ed
Shelyahin_Mihail <55269575+GervantFromRibli@users.noreply.github.com>
2020-09-08 01:09:37 +0300

    Create part of buisness logic layer
    Create some possible parts of buisness layer. It is nessesary to make some changes in database and domain entities, also in repository

---
+++ src/BuisnessLayer/BLL.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using DomainEntities;
+
+namespace BuisnessLayer
+{
+    public class BLL
+    {
+        private AreaRepository _areaRepository = new AreaRepository();
+
+        private EventAreaRepository _eventAreaRepository = new EventAreaRepository();
+
+        private EventRepository _eventRepository = new EventRepository();
+
+        private EventSeatRepository _eventSeatRepository = new EventSeatRepository();
+
+        private LayoutRepository _layoutRepository = new LayoutRepository();
+
+        private SeatRepository _seatRepository = new SeatRepository();
+
+        private VenueRepository _venueRepository = new VenueRepository();
+
+        public void AddEvent(Event @event)
+        {
+            var layout = _layoutRepository.GetAll().Select(elem => elem).Where(elem => elem.Id == @event.LayoutId).Single();
+            var venueId = _venueRepository.GetAll().Select(elem => elem).Where(elem => elem.Id == layout.VenueId).Select(elem => elem.Id).Single();
+            foreach (var eventElem in _eventRepository.GetAll())
+            {
+                var layout_check = _layoutRepository.GetAll().Select(elem => elem).Where(elem => elem.Id == eventElem.LayoutId).Single();
+                var venue = _venueRepository.GetAll().Select(elem => elem).Where(elem => elem.Id == layout.VenueId).Single();
+                if (venueId == venue.Id)
+                {
+                    throw new Exception("There is already an event");
+                }
+            }
+
+            _eventRepository.Create(@event);
+        }
+
+        public void AddVenue(Venue venue)
+        {
+            foreach (var venueElem in _venueRepository.GetAll())
+            {
+                if (venueElem.Description == venue.Description)
+                {
+                    throw new Exception("There is already a venue with such name");
+                }
+            }
+
+            _venueRepository.Create(venue);
+        }
+
+        public void AddLayout(Layout layout)
+        {
+            foreach (var layoutElem in _layoutRepository.GetAll())
+            {
+                if (layoutElem.Description == layout.Description)
+                {
+                    throw new Exception("There is already a layout with such venue");
+                }
+            }
+
+            _layoutRepository.Create(layout);
+        }
+
+        public void AddArea(Area area)
+        {
+            foreach (var areaElem in _areaRepository.GetAll())
+            {
+                if (areaElem.Description == area.Description && areaElem.LayoutId == area.LayoutId)
+                {
+                    throw new Exception("There is already a area with such layout and description");
+                }
+            }
+
+            _areaRepository.Create(area);
+        }
+
+        public void AddSeat(Seat seat)
+        {
+            foreach (var seatElem in _seatRepository.GetAll())
+            {
+                if (seatElem.AreaId == seat.AreaId && seatElem.Row == seat.Row && seatElem.Number == seat.Number)
+                {
+                    throw new Exception("There is already a seat with coords");
+                }
+            }
+
+            _seatRepository.Create(seat);
+        }
+    }
+}
