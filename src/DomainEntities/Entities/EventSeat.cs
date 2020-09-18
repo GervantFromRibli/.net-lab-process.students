5bb7d7c591d26dbe023e11a660c978d74fc5f5cc
Shelyahin Mihail <55269575+GervantFromRibli@users.noreply.github.com>
2020-09-18 22:22:19 +0300

    Some changes

--- src/DomainEntities/Entities/EventSeat.cs
+++ src/DomainEntities/Entities/EventSeat.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DomainEntities
+﻿namespace DomainEntities
 {
     // Class that represent event seat table in database
     public class EventSeat
