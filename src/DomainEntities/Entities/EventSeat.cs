5a7b1912ea3a15b42eb8c756cda764670be56598
Shelyahin Mihail <55269575+GervantFromRibli@users.noreply.github.com>
2020-09-19 20:08:46 +0300

    Revert "Some changes"
    This reverts commit 5bb7d7c591d26dbe023e11a660c978d74fc5f5cc.

--- src/DomainEntities/Entities/EventSeat.cs
+++ src/DomainEntities/Entities/EventSeat.cs
@@ -1,4 +1,10 @@
-﻿namespace DomainEntities
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainEntities
 {
     // Class that represent event seat table in database
     public class EventSeat
